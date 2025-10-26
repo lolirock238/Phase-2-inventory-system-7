a7dfa1c3f5de8b56a6bba7ab443999681499816d
Nicole kibe <nicolekibe822@gmail.com>
2025-10-26 12:35:51 +0300

    add intial comments

--- src/components/FilterButton.jsx
+++ src/components/FilterButton.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 
 export default function App() {
+  //state to store the list items
+  const [items, setItems] = useState([]);
+  //state to filter the category of the items
+  const [filter, setFilter] = useState("All");
+//fuction to add new item from Additemform
+  function handleAddItem(newItem) {
+    //adding unique id to item using date.now
+    const itemWithId = { id: Date.now(), ...newItem };
+    //use of spread operator existing items + add in of the new ones
+    setItems([...items, itemWithId]);
+  }
 
+  function handleDeleteItem(id) {
+    setItems(items.filter((item) => item.id !== id));
+  }
 
-
-
-
-
-
-
-
-
-
-
-
+  const filteredItems =
+    filter === "All" ? items : items.filter((item) => item.category === filter);
 
   return (
     <div>
